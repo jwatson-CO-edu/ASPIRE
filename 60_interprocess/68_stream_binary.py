7f1e47dbd1b587fce4983ff540a3e7f6fd50b2e6
James Watson <james.watson-2@colorado.edu>
2023-12-01 17:07:48 -0700

    INTER.PROCESS. COMM.UN.I.CA.TION!

--- 60_interprocess/68_stream_binary.py
+++ 60_interprocess/68_stream_binary.py
@@ -1,34 +1,14 @@
 # https://chase-seibert.github.io/blog/2012/11/16/python-subprocess-asynchronous-read-stdout.html
 
-import os, time, fcntl, subprocess
+import time, subprocess
 from queue import Queue
 from threading import Thread
 from time import sleep
 now = time.time
-import pbjson
-from pbjson_io import PBJSON_IO
 from random import choice, randrange
 
+from interprocess import set_non_blocking, non_block_read, PBJSON_IO
 
-########## STREAM COMMUNICATION ####################################################################
-
-def set_non_blocking( output ):
-    ''' even in a thread, a normal read with block until the buffer is full '''
-    # Original Author, Chase Seibert: https://chase-seibert.github.io/blog/2012/11/16/python-subprocess-asynchronous-read-stdout.html
-    fd = output.fileno()
-    fl = fcntl.fcntl( fd, fcntl.F_GETFL )
-    fcntl.fcntl( fd, fcntl.F_SETFL, fl | os.O_NONBLOCK )
-
-def non_block_read( output ):
-    """ Attempt a read from an un-blocked stream, If nothing received, then return empty string """
-    # Original Author, Chase Seibert: https://chase-seibert.github.io/blog/2012/11/16/python-subprocess-asynchronous-read-stdout.html
-    try:
-        out = output.read()
-        if out is None:
-            return ''
-        return out
-    except:
-        return ''
     
 
 
@@ -42,7 +22,7 @@ def character_factory():
         "str": randrange(11),
         "dex": randrange(11),
         "cha": randrange(11),
-    },
+    }
 }
 
 
@@ -50,21 +30,26 @@ def character_factory():
 
 def log_worker( stdin, stdout ):
     ''' needs to be in a thread so we can read the stdout w/o blocking '''
+
     set_non_blocking( stdout )
     bgn = now()
-    i   = 0
     pbj = PBJSON_IO()
-    while True:
+
+    while (now()-bgn) < 5.0:
+        
+        sleep( 0.050 )
+
+        ### OUTPUT ###
         try:
             cObj = pbj.pack( character_factory() )
             stdin.write( cObj )
             stdin.flush()
         except:
             pass
-        # stdin.drain()
-        print( "Sent:", i )
-        output = bytearray( non_block_read( stdout ) )
-        pbj.write( output )
+        
+        ### INPUT ###
+        inpt = non_block_read( stdout )
+        pbj.write( inpt )
         if pbj.unpack():
             msgs = pbj.get_all()
             print( "++ YES DATA ++" )
@@ -72,10 +57,16 @@ def log_worker( stdin, stdout ):
                 print( "\tProcess Said:", msg['message'] )
         else:
             print( "!! NO DATA !!" )
-        if (now()-bgn) > 10:
-            break
-        i += 1
-        sleep( 0.050 )
+
+        
+
+    print( "Asking the process to shutdown!" )
+
+    for _ in range(3):
+        cObj = pbj.pack( {"message" : "SHUTDOWN"} )
+        stdin.write( cObj )
+        stdin.flush()
+        sleep( 0.005 )
 
 
 
@@ -84,19 +75,23 @@ def log_worker( stdin, stdout ):
 if __name__ == '__main__':
 
     process = subprocess.Popen(
-        ['python3.9', 'bird2.py'],
+        ['python3.9', 'judge.py'],
         stdin  = subprocess.PIPE,
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE
     )
     sleep( 0.050 )
+    print( f"Process {process.pid} started with status: {process.returncode}" )
 
     thread = Thread( target = log_worker, 
                      args   = [process.stdin, process.stdout,] )
     thread.daemon = True
     thread.start()
 
+    # sleep( 0.050 )
+
     process.wait()
+    print( f"Process {process.pid} ended with status: {process.returncode}" )
     thread.join( timeout = 1 )
 
 
