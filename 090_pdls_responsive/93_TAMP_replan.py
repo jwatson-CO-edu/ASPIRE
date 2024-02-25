3ea1ad851399e5ea4adc977a8958792a63188db4
James Watson <james.watson-2@colorado.edu>
2024-02-25 14:58:33 -0700

    Flatmachine data upload

--- 090_pdls_responsive/93_TAMP_replan.py
+++ 090_pdls_responsive/93_TAMP_replan.py
@@ -646,7 +646,7 @@ class ResponsiveExecutive:
             # 'cea-wastar5' : Very long
             # 'ff-wastar3' : 7-15s
 
-            planner = 'ff-eager-pref' #'ff-eager-pref' # 'add-random-lazy' # 'ff-eager-tiebreak' #'goal-lazy' #'ff-eager'
+            planner = 'ff-eager' #'ff-eager-pref' # 'add-random-lazy' # 'ff-eager-tiebreak' #'goal-lazy' #'ff-eager'
             solution = solve( 
                 self.task, 
                 algorithm = "adaptive", #"focused", #"binding", #"incremental", #"adaptive", 
